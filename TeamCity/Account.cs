c97b5832e282a4ec2bde8df37e4d274e8b00dbba
Peeaks <Kristoffer2509@icloud.com>
2016-11-08 09:34:17 +0100

    This will fail a test

---
+++ TeamCity/Account.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamCity {
+    public class Account {
+        private decimal _balance;
+
+        public void Deposit(decimal amount) {
+            _balance += amount;
+        }
+
+        public void Withdraw(decimal amount) {
+            _balance -= amount;
+        }
+
+        public void TransferFunds(Account destination, decimal amount) {
+        }
+
+        public decimal Balance => _balance;
+    }
+}
---
+++ TeamCityTest/AccountTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TeamCity;
+
+namespace TeamCityTest {
+    [TestFixture]
+    class AccountTest {
+
+        [Test]
+        public void TransferFunds() {
+            Account source = new Account();
+            source.Deposit(200m);
+
+            Account destination = new Account();
+            destination.Deposit(150m);
+
+            source.TransferFunds(destination, 100m);
+
+            Assert.AreEqual(250m, destination.Balance);
+            Assert.AreEqual(100m, source.Balance);
+        }
+
+    }
+}
